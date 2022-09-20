2d5a8153577cc4e912c0ce7b1e6352c0b2e0d2ac
Sathishkumar Palanisamy <pasathishkumar@athenahealth.com>
2022-09-20 22:12:39 +0530

    output sonar url

--- jenkins/output.tf
+++ jenkins/output.tf
@@ -1,3 +1,7 @@
-output "public_dns" {
+output "jenkins_public_dns" {
     value = aws_instance.jenkins_master.public_dns
+}
+
+output "sonar_public_dns" {
+    value = aws_instance.sonar_machine.public_dns
 }
